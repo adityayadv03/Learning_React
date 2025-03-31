2353348d79855481e22cffbff5fcd222e1e1d951
Aditya Yadav <officialuser.aditya2022@gmail.com>
2025-03-31 15:32:45 +0530

    Learning how custom react works and created

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 import Chai from './chai.jsx'
 
 function App() {
+
+  const username="adityayadv03"
+
   return (
     // fragment <> we do this because we are allowed to return only one Element so we wrapped it in a fragment
     <> 
       <Chai />
-      <h2>Learning with UTube</h2>
+      <h2>Learning with UTube | {username}</h2>
       <p>Testing</p>
     </>
   )
